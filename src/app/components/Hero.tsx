047485fd60f697b67614d2533adb1d044fd2efcd
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2023-09-02 12:38:09 +0100

    Fixed line height issues in hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,10 +4,10 @@ export default function Hero() {
 
             <header>
                 <span className='block font-tertiary font-medium tracking-widest text-xl mb-8 dark:text-light-green'>Hello there, I&apos;m</span>
-                <h1 className='tracking-wide text-grayish-blue text-[clamp(2.25rem,4vw,4rem)] font-secondary mb-12 font-bold dark:text-lightest-slate'>Ayobami Tunwase.</h1>
+                <h1 className='tracking-wide text-grayish-blue text-[clamp(2.25rem,4vw,4rem)] leading-tight font-secondary mb-12 font-bold dark:text-lightest-slate'>Ayobami Tunwase.</h1>
             </header>
 
-            <h2 className='text-[clamp(2rem,3vw,3rem)] text-light-gray tracking-wide mb-8 dark:text-inherit'>Frontend web developer</h2>
+            <h2 className='text-[clamp(2rem,3vw,3rem)] leading-tight text-light-gray tracking-wide mb-8 dark:text-inherit'>Frontend web developer.</h2>
         </section>
     )
 }
